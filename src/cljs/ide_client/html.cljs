df048ee75e1d6aff5172a4e61c27e021106e73bc
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-06-19 11:52:24 +0200

    Task 70: Created row and col css file and used them on home page.

--- src/cljs/ide_client/html.cljs
+++ src/cljs/ide_client/html.cljs
@@ -1,5 +1,5 @@
 (ns ide-client.html
-  (:require [htmlcss-lib.core :refer [h2 p]]
+  (:require [htmlcss-lib.core :refer [h2 p div]]
             [ide-client.project.html :as ph]
             [ide-client.task.html :as th]
             [ide-client.working-area.html :as wah]
@@ -8,11 +8,29 @@
 (defn home-page-content
   "Home page content"
   []
-  [(h2
-     (get-label 62))
-   (p
-     (get-label 63))]
- )
+  [(div
+     [(h2
+        (get-label 62))
+      (p
+        (get-label 63))
+      ]
+     {:class "row-1-4"})
+   (div
+     [(div
+        nil
+        {:class "col-1-4"})
+      (div
+        nil
+        {:class "col-2-4 logo-hi-res"})
+      (div
+        nil
+        {:class "col-1-4"})
+      ]
+     {:class "row-2-4"})
+   (div
+     nil
+     {:class "row-1-4"})
+   ])
 
 (defn custom-menu
   "Render menu items for user that have privilege for them"
